bbe219c34dcba27559042c1b5156c74855e1b60f
priyanshug0901 <priyanshug0901@gmail.com>
2021-06-25 22:04:34 +0530

    checking push

--- backEnd/db/CRUD/controller.js
+++ backEnd/db/CRUD/controller.js
@@ -8,6 +8,24 @@ function Controller(app){
             return result;
         })
     });
+
+    // app.post('/create',(req,res)=>{
+
+    //     let id = req.body.id;
+    //     let Name = req.body.Name;
+    //     console.log(req.body);
+    //     let sql = 'INSERT INTO STU SET ?'
+    //     let posts = {id:id,Name:Name};
+    
+    //     let query = mysql.query(sql,posts,(err,result)=>{
+    //         if(err) throw err;
+    //         console.log(result);
+    //         res.send("post added");
+    //     })
+    //     // console.log(query);
+    // })
+
+
     app.post("/schedule",(req,res)=>{
         const sqlInsert="INSERT INTO tasks(task_id,name,start,ending,day,batch) VALUES('1','dsa','2021-12-20 10:00:00','2021-12-20 12:00:00','mon','batch-2');";
         db.query(sqlInsert,(err,result)=>{
